994783f1e737bcf6fa233fbac47494478781f117
root <root@ws-wagner-ubuntu.(none)>
2013-08-30 16:38:14 -0300

    modified:   src/br/com/wjaa/smstransfer/activity/ConfigActivity.java

--- src/br/com/wjaa/smstransfer/activity/ConfigActivity.java
+++ src/br/com/wjaa/smstransfer/activity/ConfigActivity.java
@@ -1,12 +1,22 @@
 package br.com.wjaa.smstransfer.activity;
 
+import roboguice.inject.ContentView;
 import br.com.wjaa.smstransfer.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+
+/**
+ * 
+ * @author Wagner Jeronimo
+ *
+ */
+@ContentView(R.layout.activity_home)
 public class ConfigActivity extends Activity {
 
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
